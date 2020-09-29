d84496198cb43fa1c44f525ed7dffdb120745e8b
thekennydied@mail.ru <12211234mc123mc>
2020-09-29 23:17:51 +0300

    add files

--- login page/login_page.js
+++ login page/login_page.js
@@ -2,11 +2,14 @@ document.getElementById('entry').onclick = function () {
 	let login = document.getElementById('login').value;
 	let password = document.getElementById('password').value;
 	let out = document.getElementById('out');
+	var snd = new Audio("audio/bababooey-sound-effect.mp3");
 
 	if (login === 'Danila' && password === '12211234' || login === 'Olesya' && password === '1928') {
 		out.innerHTML = 'Welcome';
 	}
 	else {
-		out.innerHTML = 'Bababoy';
+		out.innerHTML = 'Bababoi';
+		snd.play();
+		snd.currentTime=0;
 	}
 }
