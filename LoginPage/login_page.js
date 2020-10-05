b59842912d2a2b7b1f3057d6db1c5e69066db5bc
KennyDied <thekennydied@mail.ru>
2020-10-05 09:30:42 +0300

    add packages

---
+++ LoginPage/login_page.js
@@ -0,0 +1,17 @@
+document.getElementById('entry').onclick = function () {
+	let login = document.getElementById('login').value;
+	let password = document.getElementById('password').value;
+	let out = document.getElementById('out');
+	var snd = new Audio("audio/bababooey-sound-effect.mp3");
+
+	if (login === 'Danila' && password === '12211234' || login === 'Olesya' && password === '1928') {
+		out.innerHTML = 'Welcome';
+		window.location.href = "main_page.html";
+	}
+	else {
+		out.innerHTML = 'Bababoi';
+		snd.play();
+		snd.currentTime=0;
+		alert('Incorrect password')
+	}
+}
